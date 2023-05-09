0717919a974e04d779848ab5103f01868b1c0fa3
JhonanCaleb <jhonancalebm@gmail.com>
2023-05-08 23:49:18 -0500

    Correccion de tipo de usuario en vista

--- Views/inc/header.php
+++ Views/inc/header.php
@@ -5,8 +5,8 @@
       <i class="ph ph-user-circle"></i>
     </div>
     <div class="user__details">
-      <h1 class="user__name"><?php echo $_SESSION['names'].' '.$_SESSION['lastnames'] ?></h1>
-      <span class="user__type">ADMINISTRADOR</span>
+      <h1 class="user__name"><?php echo $_SESSION['names'] . ' ' . $_SESSION['lastnames'] ?></h1>
+      <span class="user__type"><?php echo ($_SESSION['type'] == USER_TYPE->admin) ? "ADMINISTRADOR" : (($_SESSION['type'] == USER_TYPE->superadmin) ? "SUPERADMINISTRADOR" : "VENDEDOR"); ?></span>
     </div>
   </div>
 </header>
