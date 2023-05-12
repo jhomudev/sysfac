28dfcd43e9a7b245e093dfb9c9a570faaf2517c9
JhonanCaleb <jhonancalebm@gmail.com>
2023-05-11 21:45:05 -0500

    qeliminacion de boton detalles en productos

--- Views/js/productos.js
+++ Views/js/productos.js
@@ -113,9 +113,6 @@ async function getProducts(words = "", column = "", value = "") {
               }" name="tx_product_id">
               <button class="actions__btn btn_delete" style="--cl:red;" title="Eliminar"><i class="ph ph-trash"></i></button>
             </form>
-            <button data-key="${
-              product.product_id
-            }" class="actions__btn toggleDetails" style="--cl:var(--c_green);" title="Detalles"><i class="ph ph-note"></i></button>
           </td>
         </tr>
         `;
